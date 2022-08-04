a02f95791369efa49a0ef2c05859b49bce71328b
Kunal Maurya <krsm1470@gmail.com>
2022-08-04 23:24:19 +0530

    Convert_array_into_Zig-Zag_fashion.java added

---
+++ GFG_SDE/Arrays/Convert_array_into_Zig-Zag_fashion.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            new Solution().zigZag(arr, n);
+            for (int i = 0; i < n; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution {
+    void swap(int arr[],int x,int y)
+    {
+        int temp=arr[x];
+        arr[x]=arr[y];
+        arr[y]=temp;
+    }
+    void zigZag(int arr[], int n) {
+        for(int i=1;i<n;i+=2)
+        {
+            if(arr[i-1]>arr[i])
+            {
+                swap(arr,i,i-1);
+            }
+            if(i+1<n&& (arr[i+1]>arr[i]))
+            {
+                swap(arr,i+1,i);
+            }
+        }
+    }
+
+}
